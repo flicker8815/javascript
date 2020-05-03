cf1c1f0bc487811e89f08240e57a92a3f82cd2e1
LO CHIEH <flicker8815@gmail.com>
2020-05-03 21:22:22 +0800

    Switch case

---
+++ task5/test.js
@@ -0,0 +1,7 @@
+name = prompt("Input :", '');
+if(name == 'Edge')
+	alert( "You've got the Edge!" );
+else if(name == 'Chrome' || name == 'Firefox'|| name == 'Safari'|| name == 'Opera')
+	alert( 'Okay we support these browsers too' );
+else 
+	alert( 'We hope that this page looks ok!' );
